b8f5472e34114b4538d9973f58646cdc097f52b6
Kinan Bab <babman@google.com>
2020-06-17 19:33:02 -0400

    Add c++ wrapper around pdpi for blackbox parsing of p4info files

---
+++ p4_symbolic/ir/pdpi_driver.cc
@@ -0,0 +1,58 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This is a test file for our protobuf specifications of bmv2 json.
+// It reads an input bmv2 json string (usually the output of p4c) via stdin,
+// it parses the string using protobuf, and then dumps the parsed protobuf
+// objects using protobuf text format and json.
+// The dumps are written to output files whose paths are provided as command
+// line arguments.
+
+// The implementation of our PDPI interface.
+
+#include "p4_symbolic/ir/pdpi_driver.h"
+
+#include <memory>
+
+#include "p4_pdpi/util.h"
+
+namespace p4_symbolic {
+namespace ir {
+
+// Parse into standard p4 protobuf and then into PDPI.
+// If either of these steps fail, their failure status is returned.
+absl::Status parse_pdpi(std::string p4info_path, pdpi::ir::IrP4Info *output) {
+  // Verify link and compile versions are the same.
+  GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+  // Parse the p4info file into the standard p4 protobuf.
+  p4::config::v1::P4Info p4info;
+  absl::Status status = pdpi::ReadProtoFromFile(p4info_path, &p4info);
+  if (!status.ok()) {
+    return status;
+  }
+
+  // Transform the p4 protobuf to a pdpi protobuf using a pdpi::P4InfoManager.
+  pdpi::StatusOr<std::unique_ptr<pdpi::P4InfoManager>> status_or_info =
+      pdpi::P4InfoManager::Create(p4info);
+  if (!status_or_info.ok()) {
+    return status_or_info.status();
+  }
+
+  *output = status_or_info.value()->GetIrP4Info();
+  return absl::OkStatus();
+}
+
+}  // namespace ir
+}  // namespace p4_symbolic
---
+++ p4_symbolic/ir/pdpi_driver.h
@@ -0,0 +1,45 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// This is a test file for our protobuf specifications of bmv2 json.
+// It reads an input bmv2 json string (usually the output of p4c) via stdin,
+// it parses the string using protobuf, and then dumps the parsed protobuf
+// objects using protobuf text format and json.
+// The dumps are written to output files whose paths are provided as command
+// line arguments.
+
+// Defines the signature of the driver function exposing
+// PDPI structure as a blackbox.
+
+#ifndef P4_SYMBOLIC_IR_PDPI_DRIVER_H_
+#define P4_SYMBOLIC_IR_PDPI_DRIVER_H_
+
+#include <string>
+
+#include "absl/status/status.h"
+#include "p4_pdpi/ir.h"
+
+namespace p4_symbolic {
+namespace ir {
+
+// Parse the p4info file given by "p4info_path" into a pdpi:IrP4Info
+// instance, placing the output in "output".
+// Returns ok if successful, or an appropriate failure status in case of
+// a badly formatted input file, or if the file does not exist.
+absl::Status parse_pdpi(std::string p4info_path, pdpi::ir::IrP4Info *output);
+
+}  // namespace ir
+}  // namespace p4_symbolic
+
+#endif  // P4_SYMBOLIC_IR_PDPI_DRIVER_H_
