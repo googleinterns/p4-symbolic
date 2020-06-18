cf61876f0f36b452b76f30f6eb4e3d6909808f13
Kinan Bab <babman@google.com>
2020-06-18 19:36:42 -0400

    Remove wrongly copied comments

--- p4_symbolic/util/status.cc
+++ p4_symbolic/util/status.cc
@@ -12,13 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// This is a test file for our protobuf specifications of bmv2 json.
-// It reads an input bmv2 json string (usually the output of p4c) via stdin,
-// it parses the string using protobuf, and then dumps the parsed protobuf
-// objects using protobuf text format and json.
-// The dumps are written to output files whose paths are provided as command
-// line arguments.
-
 #include "p4_symbolic/util/status.h"
 
 namespace p4_symbolic {
