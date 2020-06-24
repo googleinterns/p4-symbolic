42a1e3ae212e4b228eb9a572eb3a854ab9718b72
Kinan Bab <babman@google.com>
2020-06-24 16:00:20 -0400

    Add table entry value formatting stub with TODO

--- p4_symbolic/ir/table_entries.cc
+++ p4_symbolic/ir/table_entries.cc
@@ -15,6 +15,7 @@
 #include "p4_symbolic/ir/table_entries.h"
 
 #include <iostream>
+#include <regex>
 #include <string>
 
 #include "absl/strings/str_format.h"
@@ -23,6 +24,30 @@
 
 namespace p4_symbolic {
 namespace ir {
+
+// TODO(babman): unclear if we need this right now.
+//               Ideally, we want to represent everything as bitvectors,
+//               However, Z3 API may provide a better way to encode/transform
+//               to hex.
+pdpi::StatusOr<std::string> FormatBitStringAsHex(std::string str) {
+  // detect the format: either a number, an IP, an IP range or a mac address.
+  std::regex number("^([0-9]+)$");
+  std::regex ip("^([0-9]+)\\.([0-9]+)\\.([0-9]+)\\.([0-9]+)$");
+  std::regex ip_range("^([0-9]+)\\.([0-9]+)\\.([0-9]+)\\.([0-9]+)/([0-9]+)$");
+  std::regex mac("^(?:([0-9A-Fa-f]{2}):){5}([0-9A-Fa-f]{2})$");
+
+  std::smatch sm;
+  if (std::regex_match(str, sm, number)) {
+  } else if (std::regex_match(str, sm, ip)) {
+  } else if (std::regex_match(str, sm, ip_range)) {
+  } else if (std::regex_match(str, sm, mac)) {
+  } else {
+    return absl::Status(absl::StatusCode::kInvalidArgument,
+                        absl::StrFormat("Malformed table entry value %s", str));
+  }
+
+  return str;
+}
 
 pdpi::StatusOr<std::pair<std::string, TableEntry>> ParseLine(
     const std::string &line) {
@@ -50,10 +75,11 @@ pdpi::StatusOr<std::pair<std::string, TableEntry>> ParseLine(
       continue;
     }
 
+    ASSIGN_OR_RETURN(std::string hexstr, FormatBitStringAsHex(tokens[i]));
     if (found_delimiter) {
-      output.add_action_parameters(tokens[i]);
+      output.add_action_parameters(hexstr);
     } else {
-      output.add_match_values(tokens[i]);
+      output.add_match_values(hexstr);
     }
   }
 
