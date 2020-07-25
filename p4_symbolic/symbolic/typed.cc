8073a14e3b2c6b4229f86f55fd1b1fe1e038d852
Kinan Bab <babman@google.com>
2020-07-25 18:39:48 -0400

    Add TypedExpr class to wrap z3::expr with fixed sort and sign information. Implement basic functionality for it.

---
+++ p4_symbolic/symbolic/typed.cc
@@ -0,0 +1,125 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "p4_symbolic/symbolic/typed.h"
+
+/*
+#include "absl/status/status.h"
+#include "absl/strings/str_cat.h"
+*/
+#include "p4_symbolic/symbolic/symbolic.h"
+
+// This macro is used through out the implementation of the overloaded
+// operators for the TypedExpr class.
+// It sort checks the two operands a and b, and if they were bit vectors,
+// it will perform any needed sign conversions and size padding.
+// The macro will define two z3::expr variables "a_expr" and "b_expr",
+// corresponding to the finalized expressions of the two operands.
+#define SORT_CHECK_AND_PADD(a, b)                                 \
+  z3::expr a_expr = a.expr_;                                      \
+  z3::expr b_expr = b.expr_;                                      \
+  assert(a.sort_.sort_kind() == b.sort_.sort_kind());             \
+  if (a.sort_.is_bv()) {                                          \
+    unsigned int a_len = a.sort_.bv_size();                       \
+    unsigned int b_len = b.sort_.bv_size();                       \
+    /* If one is signed and the other is not, we will need to     \
+     * make the unsigned one signed, by padding with a single     \
+     * zero bit, since the unsigned expression must be positive.  \
+     * As an optimization, we do not need to do that padding      \
+     * explicitly, instead we only logically account for it       \
+     * increasing the size by 1, and then the later "Pad" call    \
+     * will do that sign conversion for us along the way. */      \
+    if (a.signed_ && !b.signed_) a_len += 1;                      \
+    else if(!a.signed_ && b.signed_) b_len += 1;                  \
+    unsigned int pad_size = a_len > b_len ? a_len : b_len;        \
+    a_expr = Pad(a, pad_size);                                    \
+    b_expr = Pad(b, pad_size);                                    \
+  }
+
+namespace p4_symbolic {
+namespace symbolic {
+
+namespace {
+
+z3::expr Pad(const TypedExpr &e, unsigned int size) {
+  // We should determine what kind of pad we need and apply it.
+  unsigned int diff = size - e.sort().bv_size();
+  if (diff == 0) {
+    // No padding or sign conversions needed.
+    return e.expr();
+  } else if (e.is_signed()) {
+    // We must pad, without damaging the sign bit.
+    assert(e.sort().bv_size() > 1);
+    unsigned sign_bit_index = e.sort().bv_size() - 1;
+    z3::expr sign_bit = e.expr().extract(sign_bit_index, sign_bit_index);
+    z3::expr zeros = Z3Context().bv_val(0, diff);
+    z3::expr unsign_expr = e.expr().extract(sign_bit_index - 1, 0);
+    return z3::concat(z3::concat(sign_bit, zeros), unsign_expr);
+  } else {
+    // We must pad, but no sign issues.
+    return z3::concat(Z3Context().bv_val(0, diff), e.expr());
+  }
+}
+
+}  // namespace
+
+// Copy and move assignment operators must respect pre-declared sort.
+TypedExpr& TypedExpr::operator=(const TypedExpr& other) {
+  assert(this->sort_.sort_kind() == other.sort_.sort_kind());
+  if (this->sort_.is_bv()) {
+    // We can assign an unsigned bit vector to a signed one, but not the other
+    // way around.
+    assert(this->signed_ || !other.signed_);
+    // We can only re-assign bit vectors to ones of equal or smaller size.
+    assert(this->sort_.bv_size() >= other.sort_.bv_size());
+    // Finally, if assigning an unsigned bitvector to a signed one, the size
+    // of the unsigned one has to be smaller than the signed one. Otherwise,
+    // there is no guarantee that the unsigned value interpretation will
+    // remain the same (e.g. if its most significant bit is 1).
+    if (this->signed_ && !other.signed_) {
+      assert(this->sort_.bv_size() > other.sort_.bv_size());
+    }
+    // All is good, copy other to this padding it to the proper bit length.
+    this->expr_ = Pad(other, this->sort_.bv_size());
+  } else {
+    // For other sorts, we are all good and can assign directly.
+    this->expr_ = other.expr_;
+  }
+  return *this;
+}
+
+TypedExpr& TypedExpr::operator=(TypedExpr&& other) {
+  // Same sort-checking assertions as copy assignment.
+  assert(this->sort_.sort_kind() == other.sort_.sort_kind());
+  if (this->sort_.is_bv()) {
+    assert(this->signed_ || !other.signed_);
+    assert(this->sort_.bv_size() >= other.sort_.bv_size());
+    if (this->signed_ && !other.signed_) {
+      assert(this->sort_.bv_size() > other.sort_.bv_size());
+    }
+    this->expr_ = std::move(Pad(other, this->sort_.bv_size()));
+  } else {
+    this->expr_ = std::move(other.expr_);
+  }
+  return *this;
+}
+
+// Comparison Operators.
+TypedExpr TypedExpr::operator==(const TypedExpr &b) {
+  SORT_CHECK_AND_PADD((*this), b);
+  return TypedExpr(a_expr == b_expr, this->signed_ || b.signed_);
+}
+
+}  // namespace symbolic
+}  // namespace p4_symbolic
---
+++ p4_symbolic/symbolic/typed.h
@@ -0,0 +1,88 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Defines our wrapper for typed symbolic expression.
+
+#ifndef P4_SYMBOLIC_SYMBOLIC_TYPED_H_
+#define P4_SYMBOLIC_SYMBOLIC_TYPED_H_
+
+#include "gutil/status.h"
+#include "z3++.h"
+
+namespace p4_symbolic {
+namespace symbolic {
+
+// This is very similar to z3::expr, however it stores additional type
+// information (e.g. sign), and performs type checking on copy/assignment,
+// and can unify different sized bit vectors by padding.
+//
+// Specifically, when a typed expression is declared, it can only be re-assigned
+// to z3::expr of the same matching sort (or of a bit vector sort that can be
+// padded to that sort).
+//
+// This is important for metadata fields, which may be assigned new values as
+// the symbolic program is evaluated.
+//
+// Additionally, this class overrides operators, similar to z3::expr, with
+// additional type checking (for signedness) and padding when needed.
+class TypedExpr {
+private:
+  // The underlying symbolic expression that is the current value of this
+  // typed expression.
+  z3::expr expr_;
+  // The declared sort of this typed expression. It will only accept
+  // reassignments to expressions of this same sort.
+  // Guaranteed to match the sort of expr_.
+  z3::sort sort_;
+  // Only relevant if sort_ is a bitvector. Specifies whether the bitvector
+  // is interpreted as signed (i.e. int<n> in P4) or unsigned (i.e. bit<n>
+  // in P4).
+  // Z3 does not make a distinction between these two cases at the sort level,
+  // instead, it exposed two sets of operations on bit vectors, corresponding
+  // to either cases.
+  // This class chooses which operation to apply based on this bool value.
+  bool signed_;
+
+public:
+  explicit TypedExpr(z3::expr expr, bool sign) : expr_(expr), sort_(expr.get_sort()), signed_(sign) {}
+
+  // Copyable and Movable when creating new instances.
+  TypedExpr(const TypedExpr& other) = default;
+  TypedExpr(TypedExpr&& other) = default;
+
+  // Can be re-assigned (via copy or move) as long as the re-assignment respects
+  // the declared sort.
+  TypedExpr& operator=(const TypedExpr& other);
+  TypedExpr& operator=(TypedExpr&& other);
+  
+  // Accessors
+  const z3::expr& expr() const {
+    return this->expr_;
+  }
+  const z3::sort& sort() const {
+    return this->sort_;
+  }
+  bool is_signed() const {
+    return this->signed_;
+  }
+
+  // Overloaded operators exposing corresponding z3::expr operators after sort
+  // checking and padding.
+  TypedExpr operator==(const TypedExpr &b);
+};
+
+}  // namespace symbolic
+}  // namespace p4_symbolic
+
+#endif  // P4_SYMBOLIC_SYMBOLIC_TYPED_H_
